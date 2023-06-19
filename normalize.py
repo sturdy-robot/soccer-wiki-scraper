f53fe32ae64941553a342a63451bcbe0f0c705be
sturdy-robot <pedrenriquegg@hotmail.com>
2023-06-19 11:50:04 -0300

    update: normalized data for ofm

--- normalize.py
+++ normalize.py
@@ -9,10 +9,27 @@ import json
 import os
 import re
 import asyncio
+import random
 from soccerwikiclubs import main
 
+FORMATION_STRINGS = [
+    "3-4-3",
+    "3-5-2",
+    "3-6-1",
+    "4-4-2",
+    "4-3-3",
+    "4-5-1",
+    "5-4-1",
+    "5-3-2",
+]
 
-def normalize_club_data(club: dict):
+
+def get_fifa_country_codes() -> dict:
+    with open('fifa_country_codes.json', 'r') as fp:
+        return json.load(fp)
+
+
+def normalize_club_data(club: dict, country_codes: dict):
     stadium = club["stadium"]
     pattern = re.compile(r'\((\d+|\d{1,3}(,\d{3})*)(\.\d+)?\)')
     stadium_capacity = re.findall(pattern, stadium)
@@ -22,9 +39,15 @@ def normalize_club_data(club: dict):
         stadium_capacity = int(stadium_capacity.replace(',', '').strip())
     return {
         "name": club["name"],
-        "country": club["country"],
+        "country": country_codes[club["country"]],
+        "location": club["location"],
         "stadium": stadium,
-        "stadium_capacity": stadium_capacity
+        "stadium_capacity": stadium_capacity,
+        "default_formation": random.choice(FORMATION_STRINGS),
+        "squads_def": {
+            "mu": random.randint(45, 85),
+            "sigma": random.randint(5, 35),
+        }
     }
 
 
@@ -37,12 +60,14 @@ async def normalize():
     with open('clubs.json', 'r') as fp:
         clubs = json.load(fp)
 
-        for club in clubs:
-            c = normalize_club_data(club)
-            _clubs.append(c)
+    for club in clubs:
+        c = normalize_club_data(club, get_fifa_country_codes())
+        _clubs.append(c)
+
+    clubs.sort(key=lambda x: x['country'])
 
     with open('normalized.json', 'w') as fw:
-        json.dump(_clubs, fw)
+        json.dump(_clubs, fw, indent=2)
 
 
 if __name__ == '__main__':
--- soccerwikiclubs.py
+++ soccerwikiclubs.py
@@ -1,5 +1,4 @@
 import json
-import requests
 import logging
 import asyncio
 import aiohttp
@@ -7,38 +6,55 @@ from bs4 import BeautifulSoup
 
 
 async def parse_club_data(session: aiohttp.ClientSession, url: str, name: str) -> dict | None:
-    async with session.get(url) as resp:
-        if resp.status != 200:
-            return None
-        logging.debug(f'Requesting: {url}')
-        page = await resp.text()
-        soup = BeautifulSoup(page, 'html.parser')
-        rows = soup.find_all('p', class_='player-info-subtitle mb-2')
-        manager, nickname, medium_name, short_name, year_founded, stadium, league, location, country = [
-            row.text for row in rows
-        ]
-        manager = manager.replace('Manager:', '').strip()
-        nickname = nickname.replace('Nickname:', '').strip()
-        medium_name = medium_name.replace('Medium Name:', '').strip()
-        short_name = short_name.replace('Short Name:', '').strip()
-        year_founded = year_founded.replace('Year Founded:', '').strip()
-        stadium = stadium.replace('Stadium:', '').strip()
-        league = league.replace('League:', '').strip()
-        location = location.replace('Location:', '').strip()
-        country = country.replace('Country:', '').strip()
+    retries = 3
+    delay = 1
+    for _ in range(retries):
+        try:
+            async with session.get(url, timeout=aiohttp.ClientTimeout(total=1000)) as resp:
+                logging.debug(f'Getting the {name} data')
+                if resp.status != 200:
+                    logging.error(f'{name}: {url} returned {resp.status}')
+                else:
+                    logging.debug(f'Requesting: {url}')
+                    page = await resp.text()
+                    soup = BeautifulSoup(page, 'html.parser')
+                    rows = soup.find_all('p', class_='player-info-subtitle mb-2')
+                    manager, nickname, medium_name, short_name, year_founded, stadium, league, location, country = [
+                        row.text for row in rows
+                    ]
+                    manager = manager.replace('Manager:', '').strip()
+                    nickname = nickname.replace('Nickname:', '').strip()
+                    medium_name = medium_name.replace('Medium Name:', '').strip()
+                    short_name = short_name.replace('Short Name:', '').strip()
+                    year_founded = year_founded.replace('Year Founded:', '').strip()
+                    stadium = stadium.replace('Stadium:', '').strip()
+                    league = league.replace('League:', '').strip()
+                    location = location.replace('Location:', '').strip()
+                    country = country.replace('Country:', '').strip()
 
-        return {
-            "name": name,
-            "manager": manager,
-            "nickname": nickname,
-            "medium_name": medium_name,
-            "short_name": short_name,
-            "year_founded": year_founded,
-            "stadium": stadium,
-            "league": league,
-            "location": location,
-            "country": country,
-        }
+                    logging.debug(f'Creating the dictionary for {name}')
+                    return {
+                        "name": name,
+                        "manager": manager,
+                        "nickname": nickname,
+                        "medium_name": medium_name,
+                        "short_name": short_name,
+                        "year_founded": year_founded,
+                        "stadium": stadium,
+                        "league": league,
+                        "location": location,
+                        "country": country,
+                    }
+        except asyncio.TimeoutError:
+            logging.error(f"Request timed out {name}: {url}")
+        except aiohttp.ClientError as e:
+            logging.error(f"Request {url} failed with exception {e}")
+
+        await asyncio.sleep(delay)
+    logging.error(f"Request for {name} failed after multiple attempts")
+    return None
+
+
 
 
 async def combine_club_data(clubs: list[dict]) -> list[dict]:
@@ -49,7 +65,7 @@ async def combine_club_data(clubs: list[dict]) -> list[dict]:
             club_id = club["ID"]
             logging.debug(f'{club_id}: {club["Name"]}')
             url = f'https://en.soccerwiki.org/squad.php?clubid={club_id}'
-            tasks.append(asyncio.ensure_future(parse_club_data(session, url, club["Name"])))
+            tasks.append(asyncio.create_task(parse_club_data(session, url, club["Name"])))
 
         _clubs = await asyncio.gather(*tasks)
 
@@ -64,7 +80,7 @@ async def main():
         clubs = data['ClubData']
         _clubs = await combine_club_data(clubs)
         with open('clubs.json', 'w') as fw:
-            json.dump(_clubs, fw)
+            json.dump(_clubs, fw, indent=2)
 
 
 if __name__ == '__main__':
